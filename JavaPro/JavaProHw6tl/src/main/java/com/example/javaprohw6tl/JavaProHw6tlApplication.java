0c02b3750e9711309e5af3e96892ff9277376087
ILowriderI <126571170+ILowriderI@users.noreply.github.com>
2023-04-18 14:36:51 +0300

    add files

---
+++ JavaPro/JavaProHw6tl/src/main/java/com/example/javaprohw6tl/JavaProHw6tlApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprohw6tl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProHw6tlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProHw6tlApplication.class, args);
+    }
+
+}
---
+++ JavaPro/JavaProHw6tl/src/test/java/com/example/javaprohw6tl/JavaProHw6tlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaprohw6tl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProHw6tlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
