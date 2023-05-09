fd7359b141ee036a15b077c0f819bcc213ca2605
ILowriderI <126571170+ILowriderI@users.noreply.github.com>
2023-05-09 14:38:21 +0300

    add files

---
+++ JavaPro/JavaProHw9/src/main/java/com/example/javaprohw9/JavaProHw9Application.java
@@ -0,0 +1,13 @@
+package com.example.javaprohw9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProHw9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProHw9Application.class, args);
+    }
+
+}
